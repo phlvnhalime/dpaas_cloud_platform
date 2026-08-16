9c517b388b7b00b214f12f7365bffad6b20ced55
Halime <halimephlvn@icloud.com>
2026-08-16 16:53:28 +0200

    feat: attach keypair and security group to Terraform VM

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -4,13 +4,21 @@
     Size - reuse Day 3 data lookup
     attached to private network
     print useful values after apply
+
+    "----Security Groups-----"
+    "key_pair" = Which OpenStack keypair to inject
+    "security_groups" = Which SG name(s) apply to this VM
 */
 
 resource "openstack_compute_instance_v2" "lab" {
     name = "dpaas-tf-vm"
     image_id = data.openstack_images_image_v2.cirros.id
     flavor_id = data.openstack_compute_flavor_v2.small.id
+    key_pair = openstack_compute_keypair_v2.lab.name
 
+    security_groups = [
+        openstack_networking_secgroup_v2.lab.name,
+    ]
     network {
         uuid = data.openstack_networking_network_v2.private.id
     }
